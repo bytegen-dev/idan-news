a5958cf687fb37880d0e1e169d7d1686ab207889
tertiux44 <secondsonofadebayo@gmail.com>
2023-06-11 06:42:10 +0100

    finished already

--- src/pages/Oops.jsx
+++ src/pages/Oops.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Oops() {
   return (
     <div className='oops'>
-      Oops
+      <span>404</span>
+      <p>Page Not Found</p>
+      <a href="https://wa.me/+2347035658853" target="_blank" rel='noreferrer'>Contact support</a>
     </div>
   )
 }
